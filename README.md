0395304c9b58c3fc760c92f60f63197fe5e188bb
Andrew McOlash <amcolash@gmail.com>
2018-08-26 00:17:27 -0700

    Add in maps, fixes, etc

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 A plugin for lightgallery which adds an exif panel
 
 ## Attributions
-- jquery based lightweight photo gallery: [lightgallery](http://sachinchoolur.github.io/lightGallery)
+- jQuery based lightweight photo gallery: [lightgallery](http://sachinchoolur.github.io/lightGallery)
+- Simple js map library: [LeafletJS](https://leafletjs.com/)
 - Exif parsing library: [exif-js](https://github.com/exif-js/exif-js)
 - RobG for his take on exif dates: [stackoverflow](https://stackoverflow.com/questions/43083993)
--- src/js/lg-exif.js
+++ src/js/lg-exif.js
@@ -1,4 +1,4 @@
-(function ($, window, document, undefined) {
+(function ($) {
 
     'use strict';
 
@@ -34,8 +34,14 @@
                 data = "1/" + Math.round(1 / parseFloat(data));
             } else if (value === "DateTimeOriginal") {
                 data = this.parseDate(data).toLocaleString();
+            } else if (value === "GPSLatitude" && this.exif.Geo) {
+                data = this.exif.Geo[0].toFixed(5);
+                suffix = " " + this.exif.GPSLatitudeRef;
+            } else if (value === "GPSLongitude" && this.exif.Geo) {
+                data = this.exif.Geo[1].toFixed(5);
+                suffix = " " + this.exif.GPSLongitudeRef;
             } else if (!isNaN(data)) {
-                data = parseFloat(data);
+                data = +parseFloat(data).toFixed(2);
             }
 
             this.html += '<li>' + label + prefix + data + suffix + '</li>';
@@ -55,42 +61,73 @@
         this.appendData("Make");
         this.appendData("Model");
 
+        if (this.exif.Geo) {
+            this.html += '<hr>';
+            this.appendData("GPSLatitude", "Latitude");
+            this.appendData("GPSLongitude", "Longitude");
+            this.appendData("GPSAltitude", "Altitude", "", " m");
+        }
+
         this.html += '</ul>';
+
+        if (this.exif.Geo) {
+            this.html += '<div id="map"></div>';
+        }
+
         $('.lg-exif .content').html(this.html);
+
+        if (this.exif.Geo) {
+            var map = L.map('map').setView(this.exif.Geo, 15);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+            L.marker(this.exif.Geo).addTo(map);
+        }
     }
 
     Exif.prototype.update = function() {
         const _this = this;
-        const img = $(this.core.el).find("img").get(this.core.index);
+        const thumbnail = this.core.$items.get(this.core.index);
+        const img = $(thumbnail).find('img').get(0);
 
         EXIF.getData(img, function () {
             _this.exif = EXIF.getAllTags(this);
+            console.log(_this.exif);
+            _this.exif.Geo = _this.updateGeo();
             _this.exif.Filename = img.alt;
-            _this.exif.SourceResolution = _this.exif.PixelXDimension + "x" + _this.exif.PixelYDimension;
+
+            // Keeping this out for now since it is not reliable, might need a mix of things for resolution
+            // _this.exif.SourceResolution = _this.exif.PixelXDimension + "x" + _this.exif.PixelYDimension;
+
             _this.renderExif();
         });
     }
 
     Exif.prototype.init = function () {
-        console.log(this);
+        // Define the base panel template
+        const template = '<div class="lg-exif hidden"><div><h3>Image Information</h3>\
+        <span class="lg-hide lg-icon"></span></div><hr><div class="content"></div></div>';
+
+        // Inject template into lg + get a reference to the root
+        this.core.$outer.prepend(template);
+        this.element = this.core.$outer.find('.lg-exif');
         
-        $('.lg').parent().prepend('<div class="lg-exif hidden"><div><h3>Image Information</h3>\
-        <span class="lg-hide lg-icon"></span></div><hr><div class="content"></div></div>');
-        
+        // Add slide event listener
         const _this = this;
-        $('#media').on('onAfterSlide.lg', function() {
-            console.log("update");
+        this.$el.on('onAfterSlide.lg', function() {
             _this.update();
         });
 
-        if ($('.lg').has('.lg-toolbar')) {
-            const close = $('.lg-toolbar .lg-close');
-            close.after('<span class="lg-icon lg-info"></span>');
-            $('.lg-toolbar .lg-info, .lg-exif .lg-hide').on('click', function () {
-                $('.lg-exif').toggleClass('hidden');
-            });
-        }
+        // Inject info button before close button
+        const close = this.core.$outer.find('.lg-toolbar .lg-close');
+        close.after('<span class="lg-icon lg-info"></span>');
 
+        // Attach toggle handler to info icon + close button (in panel)
+        this.core.$outer.find('.lg-toolbar .lg-info, .lg-exif .lg-hide').on('click', function () {
+            _this.element.toggleClass('hidden');
+        });
+
+        // Update info based on current image
         this.update();
     }
 
@@ -104,16 +141,34 @@
         return new Date(b[0], b[1] - 1, b[2], b[3], b[4], b[5]);
     }
 
+    Exif.prototype.updateGeo = function() {
+        if (this.exif.GPSLatitude && this.exif.GPSLongitude) {
+            return [this.parseDMS(this.exif.GPSLatitude), this.parseDMS(this.exif.GPSLongitude)];
+        }
+
+        return null;
+    }
+
+    /* Take d/m/s array from exif-js and convert to decimal format */
+    Exif.prototype.parseDMS = function(input) {
+        if (input.length === 3) {
+            return input[0] + input[1] / 60 + input[2] / 3600;
+        }
+
+        // Not in the format we expected
+        return input;
+    }
+
     /**
     * Destroy function must be defined.
     * lightgallery will automatically call your module destroy function 
     * before destroying the gallery
     */
     Exif.prototype.destroy = function () {
-        $('.lg-exif').remove();
+        this.element.remove();
     }
 
-    // Attach your module with lightgallery
+    // Attach your module to lightgallery
     $.fn.lightGallery.modules.exif = Exif;
 
 
