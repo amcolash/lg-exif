cea9539dca485e4be67094834d511b2cb35bea35
Andrew McOlash <amcolash@gmail.com>
2018-10-13 15:11:38 -0700

    Fix gulpfile for gulp 4 upgrade, clean things up

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,8 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
 const uglifycss = require('gulp-uglifycss');
 const del = require('del');
-const runSequence = require('run-sequence');
 
 // Lots o' constants
-
 const baseName = 'lg-exif';
 const dest = 'dist';
 
@@ -17,25 +15,10 @@ const jsDest = dest + '/js';
 const cssFiles = 'src/css/**/*.css';
 const cssDest = dest + '/css';
 
-const imageFiles = 'src/images/**/*.{png,jpg}';
-const imageDest = dest + '/images';
-
-const fontFiles = 'src/fonts/**/*.{ttf,woff,eot,svg}';
-const fontDest = dest + '/fonts';
-
-// Meta Tasks
-
-gulp.task('default', () => runSequence('clean', 'dist'));
-
-gulp.task('dist', ['minify', 'copy']);
-gulp.task('minify', ['scripts', 'styles']);
-gulp.task('copy', ['copy_fonts', 'copy_images']);
-
+// Actual tasks
 gulp.task('watch', function () {
     gulp.watch('src/**/*', ['default'])
 });
-
-// Actual tasks
 
 gulp.task('scripts', function () {
     return gulp.src(jsFiles)
@@ -55,16 +38,11 @@ gulp.task('styles', function () {
         .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('copy_fonts', function () {
-    return gulp.src(fontFiles)
-        .pipe(gulp.dest(fontDest));
-});
-
-gulp.task('copy_images', function () {
-    return gulp.src(imageFiles)
-        .pipe(gulp.dest(imageDest));
-});
-
 gulp.task('clean', function () {
     return del(dest + '/**', { force: true });
 });
+
+// Meta Tasks
+gulp.task('minify', gulp.parallel('scripts', 'styles'));
+gulp.task('default', gulp.series('clean', 'minify'));
+
